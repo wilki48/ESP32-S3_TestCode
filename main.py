e6149c0c6a105e07e15bea7a072fbbab95f9f9c5
Gary Wilkinson <wilki48@gmail.com>
2024-03-09 18:29:51 -0800

    22222

--- main.py
+++ main.py
@@ -53,15 +53,15 @@ def checkForUpdates():
     #ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota.py")
     #ota_updater.download_and_install_update_if_available()
     
-    sleep(0.25)
-    machine.reset()  # Reset the device to run the new code.
     
 if __name__ == "__main__":
     try:
         setupNetwork()
         #get_ntp_time()
         get_mem_info()
-        checkForUpdates()
+        if (checkForUpdates() == True):
+            sleep(0.25)
+            machine.reset()  # Reset the device to run the new code.
         
         
     except KeyboardInterrupt:
